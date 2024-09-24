72df34d772aee8ee951eb5b3dc07c3c6a4ce5fe9
nobless-21 <alexandr.sukhorukov2005@gmail.com>
2024-09-24 15:43:15 +0300

    back and front

--- 236.py
+++ 236.py
@@ -1,3 +1,5 @@
+from front import *
+
 def sqr_func(a: float, b:float, c: float):
     D = float(b ** 2 - 4*a*c)
     if D < 0:
@@ -9,8 +11,31 @@ def sqr_func(a: float, b:float, c: float):
     else:
         return x1
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = sqr_func(a,b,c)
-print(d)
+def count_sqr(a: float, b:float, c: float):
+    D = float(b ** 2 - 4*a*c)
+    x1 = (-b + D ** (1/2))/(2*a)
+    x2 = (-b - D ** (1 / 2)) /(2 * a)
+    if (x1 != x2):
+        return 2
+    else:
+        return 1
+
+
+hello()
+
+def main():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = sqr_func(a,b,c)
+    if (d == -1):
+        error()
+    else:
+        if (count_sqr(a,b,c) == 2):
+            two_sqrt()
+        else:
+            one_sqrt()
+        print(d)
+
+if __name__ == "__main__":
+    main()
